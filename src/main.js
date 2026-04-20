b5e33e73dc63df05b5b7da7b69a34b7d31ff9355
grisha <derbe.astemir@mail.ru>
2026-04-20 18:23:16 +0300

    API log weather

--- src/main.js
+++ src/main.js
@@ -1,11 +1,8 @@
 const apiKey = "f40569378f0426e07a2fee36dc158f8c";
 
-// ПОГОДА ПО ШИРОТЕ И ДОЛГОТЕ
 
-let latValue = 44.34;
-let lonValue = 10.99;
-
-fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latValue}&lon=${lonValue}&appid=${apiKey}`)
+// searchBtn.addEventListener("submit", () => {
+fetch(`https://api.openweathermap.org/data/2.5/weather?q=${"Краснодар"}&appid=${apiKey}`) 
     .then((response) => {
         console.log("response:", response);
 
@@ -13,27 +10,12 @@ fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latValue}&lon=${lon
     })
     .then((json) => {
         console.log(json);
-        const weather = json
-        console.log(weather)
+        const { name, weather,  main } = json
+        console.log(weather, main)
+        cityTitle.innerText = name;
+        tempTitle.innerText = Math.ceil(main.temp - 273.15);
     })
 
 
-// ГЕОКОДЕР ГОРОДОВ 
-
-let searchInputValue = document.getElementById("searchInput").value;
-let searchBtn = document.getElementById("searchBtn");
-
-searchBtn.addEventListener("submit", () => {
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${searchInputValue}&appid=${apiKey}`) 
-        .then((response) => {
-            console.log("response:", response);
-
-            return response.json()
-        })
-        .then((json) => {
-            console.log(json);
-            const { lat, lon } = json
-            latValue, lonValue = lat, lon
-            console.log(lat, lon)
-        })
-})
+let cityTitle = document.getElementById("mainWeatherTitle");
+let tempTitle = document.getElementById("mainWeatherTemperature");
